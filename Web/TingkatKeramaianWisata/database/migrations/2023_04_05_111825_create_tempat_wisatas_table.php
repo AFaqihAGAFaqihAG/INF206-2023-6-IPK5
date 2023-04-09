2f0c28020a08855096ad586a3eae81064a265a2d
AFaqihAGAFaqihAG <a_faqih@mhs.unsyiah.ac.id>
2023-04-09 16:34:29 +0700

    add landing page

--- Web/TingkatKeramaianWisata/database/migrations/2023_04_05_111825_create_tempat_wisatas_table.php
+++ Web/TingkatKeramaianWisata/database/migrations/2023_04_05_111825_create_tempat_wisatas_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+    
 return new class extends Migration
 {
     /**
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
