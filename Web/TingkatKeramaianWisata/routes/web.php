add5b5c0282260429faef9010edc333f9772ae5d
Ahmad Naziel Firdaus <ahmad9@mhs.unsyiah.ac.id>
2023-04-09 18:16:01 +0700

    add login dan dan daftar

--- Web/TingkatKeramaianWisata/routes/web.php
+++ Web/TingkatKeramaianWisata/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('LandingPage');
 });
+
+Route::get('login', function () {
+    return view('LoginPage');
+});
+
+Route::get('daftar', function () {
+    return view('DaftarPage');
+});
