8b622bee40b8171f722c5ec11b60aa3d579494de
Ivan chiari <Ivan31@mhs.unsyiah.ac.id>
2023-04-13 20:57:23 +0700

    feat: buat fungsi mendaftar petugas

--- Web/TingkatKeramaianWisata/routes/web.php
+++ Web/TingkatKeramaianWisata/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\PetugasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +24,8 @@ Route::get('login', function () {
 Route::get('daftar', function () {
     return view('DaftarPage');
 });
+Route::post('/daftar-petugas', [PetugasController::class, 'daftarPetugas']);
+
 Route::get('pengunjung', function () {
     return view('PengunjungPage');
 });
