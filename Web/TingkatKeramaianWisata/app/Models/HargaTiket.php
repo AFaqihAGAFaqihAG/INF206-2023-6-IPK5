a2a586ad704f41d50cb155b9350687e95e47246b
Ivan chiari <Ivan31@mhs.unsyiah.ac.id>
2023-05-10 23:32:17 +0700

    feat: edit harga tiket dan isi nya oleh petugas

--- Web/TingkatKeramaianWisata/app/Models/HargaTiket.php
+++ Web/TingkatKeramaianWisata/app/Models/HargaTiket.php
@@ -11,6 +11,15 @@ class HargaTiket extends Model
 
     protected $table = 'harga_tiket';
 
+    protected $primaryKey = 'id_harga_tiket';
+
+    protected $fillable = [
+        'id_tempat_wisata',
+        'jenis_tiket',
+        'harga',
+        'sisa_jumlah',
+    ];
+
     public function tempatWisata()
     {
         return $this->belongsTo(TempatWisata::class, 'id_tempat_wisata');
