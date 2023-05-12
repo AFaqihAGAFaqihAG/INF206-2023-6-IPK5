b2803db5730dc93ef29cf14c9936848bb9847dac
Ivan chiari <Ivan31@mhs.unsyiah.ac.id>
2023-05-12 08:20:02 +0700

    fix: login error

--- Web/TingkatKeramaianWisata/app/Http/Controllers/LoginController.php
+++ Web/TingkatKeramaianWisata/app/Http/Controllers/LoginController.php
@@ -22,41 +22,38 @@ class LoginController extends Controller
             'email' => 'required|email',
             'password' => 'required',
         ]);
-
+    
         $credentials = $request->only('email', 'password');
-
+    
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('dashboard');
-        }
-
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Username atau password salah.',
-        ]);
-        
-
-        // Coba untuk melakukan login
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = Auth::user();
-
+    
             // Validasi apakah petugas memiliki id_tempat_wisata yang terdaftar pada tabel tempat_wisata
             $tempatWisata = DB::table('tempat_wisata')
                 ->where('id_tempat', '=', $user->id_tempat_wisata)
                 ->first();
-
+    
             if ($tempatWisata) {
-                // Jika berhasil login, redirect ke halaman tempat wisata yang terkait
-                return redirect()->route('petugas.show', ['id_tempat' => $user->id_tempat_wisata]);
+                // Jika berhasil login, kirimkan URL halaman tempat wisata yang terkait sebagai response
+                return response()->json([
+                    'status' => 'success',
+                    'url' => route('petugas.show', ['id_tempat' => $user->id_tempat_wisata]),
+                ]);
             } else {
-                // Jika id_tempat_wisata yang terkait tidak terdaftar pada tabel tempat_wisata, logout petugas dan redirect ke halaman login dengan pesan error
+                // Jika id_tempat_wisata yang terkait tidak terdaftar pada tabel tempat_wisata, logout petugas dan kirimkan pesan error sebagai response
                 Auth::logout();
-                return redirect()->back()->withErrors(['email' => 'Anda tidak memiliki akses ke halaman tersebut']);
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Anda tidak memiliki akses ke halaman tersebut',
+                ]);
             }
         } else {
-            // Jika gagal login, kembali ke halaman login dengan pesan error
-            return redirect()->back()->withErrors(['email' => 'Email atau password salah']);
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Username atau password salah',
+            ]);
         }
-    }
+    }    
 
     // Proses logout
     public function logout()
